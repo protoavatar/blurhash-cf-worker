644f7ac973abdcf760da20750e62835542af4901
protoavatar <protoavatar@skiff.com>
2024-11-09 08:06:27 -0300

    blurhash with kv working

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # blurhash cloudflare workers
+
 This is a simple cloudflare worker that uses blurhash to generate a placeholder image for a given image url.
 
 [demo](https://blurhash-cf-worker.pages.dev/)
@@ -6,11 +7,13 @@ This is a simple cloudflare worker that uses blurhash to generate a placeholder 
 ## Build wasm with rust
 
 docker build image
+
 ```shell
 docker build -t rust-wasm .
 ```
 
 build wasm
+
 ```shell
 docker run --rm -v $(pwd):/usr/src/app rust-wasm
 ```
@@ -18,5 +21,13 @@ docker run --rm -v $(pwd):/usr/src/app rust-wasm
 ## Run server
 
 ```shell
-cd worker && yarn dev
+cd worker && pnpm dev
 ```
+
+Agregue la opcion de guardar los blurhashes en una dB KV en cloudflare, para guardar aquellas que ya calcule y obtenerlas mas rapido.
+Para eso le paso el id de la foto como parametro y la url (Si no hay id usa la URL como key). Por otro lado hay que pasarle un parametro token con la url
+Implementacion KV: https://developers.cloudflare.com/kv/get-started/
+implementacion secret: https://developers.cloudflare.com/workers/configuration/secrets/
+
+Use the command 'npx wrangler secret put --env production TOKEN' to deploy a securely stored token to Cloudflare. With this command, you will be prompted to enter a random secret value, which will be used to authenticate your requests with the HTTP Authorization header as described below. You can also set this encrypted value directly in your Cloudflare dashboard.
+Deploy the worker with 'npm run deploy'
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,25 +1,76 @@
 import { encode_image } from './blurhash';
 
-interface Env {}
+interface Env {
+	blurhashesabici: KVNamespace;
+	TOKEN: string;
+}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
+		const token = url.searchParams.get('token');
+		// Strict check for token existence
+		if (!env.TOKEN || env.TOKEN.length === 0) {
+			return new Response('You must set the TOKEN environment variable.', {
+				status: 401,
+			});
+		}
+		if (token !== env.TOKEN) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+
 		const imageUrl = url.searchParams.get('img');
+		const imageId = url.searchParams.get('id');
 		if (!imageUrl) {
 			return new Response('img query parameter is required', { status: 400 });
 		}
 
-		const response = await fetch(imageUrl);
-		const buffer = await response.arrayBuffer();
-		const image = new Uint8Array(buffer);
-		const hash = encode_image(image, 6, 4);
+		try {
+			const value = await env.blurhashesabici.get(imageId || imageUrl);
+			if (value === null) {
+				console.log(`No blurhash found for ${imageId || imageUrl}, constructing it`);
+				console.log('Image URL:', imageUrl);
+				const response = await fetch(imageUrl);
+				console.log('Response status:', response.status);
+				const buffer = await response.arrayBuffer();
+				const image = new Uint8Array(buffer);
+				const hash = encode_image(image, 6, 4);
+				console.log('Hash:', hash);
+				const key = await env.blurhashesabici.put(imageId || imageUrl, hash);
 
-		return new Response(hash, {
-			status: 200,
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-		});
+				console.log('Key:', key);
+				return new Response(hash, {
+					status: 200,
+					headers: {
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+						'Access-Control-Allow-Headers': 'Content-Type',
+					},
+				});
+			} else {
+				console.log(`Blurhash found for ${imageId || imageUrl}, constructing it`, value);
+
+				return new Response(value, {
+					status: 200,
+					headers: {
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+						'Access-Control-Allow-Headers': 'Content-Type',
+					},
+				});
+			}
+		} catch (err) {
+			// In a production application, you could instead choose to retry your KV
+			// read or fall back to a default code path.
+			console.error(`KV returned error: ${err}`);
+			return new Response('', {
+				status: 200,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+					'Access-Control-Allow-Headers': 'Content-Type',
+				},
+			});
+		}
 	},
 };
